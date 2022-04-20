eaecf6681bbf0606206f7fc2ad773561f2fa1ed4
Jubair Zaman <47445577+jubairzaman@users.noreply.github.com>
2022-04-20 13:55:36 +0600

    Add files via upload

---
+++ dbConnection.php
@@ -0,0 +1,16 @@
+<?php
+    $db_host = 'localhost' ;
+    $db_user =  'root' ;
+    $db_password = "" ;
+    $db_name = "userlogin" ;
+    $db_port= 3306 ;
+    
+
+/*/create connection */
+$conn = new mysqli($db_host, $db_user,$db_password,$db_name,$db_port) ;
+
+/*checking Connection*/
+   
+
+
+?>
